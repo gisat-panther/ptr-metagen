c1c105f431393aedaa40c8ed5411c3182248b202
micha <michal.opletal@gisat.cz>
2022-04-04 16:24:40 +0200

    fix encoding in load method

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ptrmetagen",
-    version="1.0.2",
+    version="1.0.3",
     description="Package for generation of metastructures for Panter project",
     long_description=README,
     long_description_content_type="text/markdown",
