292a0c1e235392bf606803da2a5a07a8e863ffe1
micha <michal.opletal@gisat.cz>
2022-04-12 09:57:44 +0200

    view generation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ptrmetagen",
-    version="1.0.4",
+    version="1.0.5",
     description="Package for generation of metastructures for Panter project",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -22,6 +22,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7"],
-    packages=["metagen"],
+    packages=["metagen","metagen.components"],
     include_package_data=True,
     )
