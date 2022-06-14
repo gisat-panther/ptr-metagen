6525d1292f47ddf0e10a401e845accb1aba448a4
micha <michal.opletal@gisat.cz>
2022-06-14 22:33:10 +0200

    utils geometry

--- metagen/__init__.py
+++ metagen/__init__.py
@@ -2,4 +2,4 @@ from metagen.generator import Generator
 from metagen.metadata import Application, Configuration, Scope, Place, Period, LayerTemplate, Attribute, SpatialWMS, \
     RelationSpatial, SpatialAttribute, SpatialWMTS, SpatialVector, Style, Case, RelationAttribute, Tag
 
-__version__ = '1.1.7'
+__version__ = '1.1.8'
